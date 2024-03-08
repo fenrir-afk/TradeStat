3d39c416325bb1f75f7a6f34442c1705fc34afac
Stepan <fenrird@inbox.ru>
2024-03-08 10:18:32 +0300

    Little optimization

--- app/src/main/java/com/example/tradestat/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/tradestat/ui/home/HomeViewModel.kt
@@ -1,5 +1,4 @@
 package com.example.tradestat.ui.home
-
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
@@ -12,24 +11,20 @@ import com.example.tradestat.data.model.Strategy
 import com.example.tradestat.data.model.Trade
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
-
     val getNumberList:MutableLiveData<List<Int>> = MutableLiveData()
     var shortNumber: Int = 0
     var longNumber: Int = 0
     var winNumber: Int = 0
     var defeatNumber: Int = 0
     private val repository:TradesRepository
-
     init {
         val tradeDao = TradeDatabase.getDatabase(application).getTradeDao()
         val strategyDao = TradeDatabase.getDatabase(application).getStrategyDao()
         val instrumentDao = TradeDatabase.getDatabase(application).getInstrumentDao()
         repository = TradesRepository(tradeDao,strategyDao,instrumentDao)
-        updateNumbers()
+        //updateNumbers()
     }
-
    fun updateNumbers(){
         viewModelScope.launch(Dispatchers.IO) {
             shortNumber = repository.getShortPos()
