171c3d91bb28b72f9855d1969044b4b8126d0307
Stepan <Fenrird@inbox.ru>
2024-04-22 11:37:45 +0300

    Change graph for strategy card

--- app/src/main/java/com/example/tradestat/ui/strategyStatistic/StrategyActivity.kt
+++ app/src/main/java/com/example/tradestat/ui/strategyStatistic/StrategyActivity.kt
@@ -3,55 +3,81 @@ package com.example.tradestat.ui.strategyStatistic
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.tradestat.R
 import com.example.tradestat.databinding.ActivityStrategyBinding
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.data.RadarData
 import com.github.mikephil.charting.data.RadarDataSet
 import com.github.mikephil.charting.data.RadarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet
+import java.util.ArrayList
+import java.util.Arrays
 
 class StrategyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStrategyBinding
-    val profileValues = ArrayList<RadarEntry>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStrategyBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        dataListing()
+        setChart()
+
     }
 
-    private fun dataListing() {
-        profileValues.add(RadarEntry(150f))
-        profileValues.add(RadarEntry(163f))
-        profileValues.add(RadarEntry(120f))
-        profileValues.add(RadarEntry(3150f))
-        profileValues.add(RadarEntry(165f))
-        setChart()
-    }
+
 
     private fun setChart() {
-        val dataSet = RadarDataSet(profileValues,"Radar chard legend")
-        dataSet.valueTextColor = Color.GRAY
-        dataSet.valueTextSize = 12f
-        dataSet.fillColor = resources.getColor(R.color.darkWhite)
-        dataSet.color = resources.getColor(R.color.darkWhite)
-        dataSet.lineWidth = 4f
-        dataSet.setDrawHighlightIndicators(true)
-        dataSet.setDrawFilled(true)
-        dataSet.setDrawValues(true)
+        // Получение левой оси
+        val leftAxis = binding.chart.axisLeft
+        leftAxis.textColor = ContextCompat.getColor(this, R.color.lightGray)
 
-        val data = RadarData(dataSet)
-        data.setValueTextColor(Color.GRAY)
-        binding.bubbleChart.data = data
-        binding.bubbleChart.invalidate()
-        binding.bubbleChart.legend.isEnabled = false
-        binding.bubbleChart.description.isEnabled = false
-        binding.bubbleChart.setNoDataText("No data available")
-        binding.bubbleChart.animateXY(1400,1400)
-        binding.bubbleChart.setTouchEnabled(true)
-        binding.bubbleChart.setBackgroundColor(Color.BLACK)
-        binding.bubbleChart.webColor = Color.WHITE
-        binding.bubbleChart.xAxis.textColor = Color.GRAY
-        binding.bubbleChart.yAxis.textColor = Color.GRAY
+        // Получение правой оси
+        val rightAxis = binding.chart.axisRight
+        rightAxis.textColor = ContextCompat.getColor(this, R.color.lightGray)
 
+        val xAxis = binding.chart.xAxis
+        xAxis.textColor = ContextCompat.getColor(this, R.color.lightGray)
+
+        val legend = binding.chart.legend
+        legend.textColor = ContextCompat.getColor(this, R.color.lightGray)
+
+        val description = Description()
+        description.text = ""
+        description.textColor = resources.getColor(R.color.lightGray)
+
+
+        var list1 = ArrayList<Entry>();
+        list1.add(Entry(0f,10f))
+        list1.add(Entry(1f,10f))
+        list1.add(Entry(2f,15f))
+        list1.add(Entry(3f,20f))
+
+        var list2 = ArrayList<Entry>();
+        list2.add(Entry(0f,5f))
+        list2.add(Entry(1f,20f))
+        list2.add(Entry(2f,25f))
+        list2.add(Entry(3f,30f))
+
+
+        val lineDataSet1 = LineDataSet(list1,"First")
+        lineDataSet1.color = Color.GREEN
+        lineDataSet1.setCircleColor(Color.GREEN)
+        lineDataSet1.valueTextColor = Color.GREEN
+
+        val lineDataSet2 = LineDataSet(list2,"Second")
+
+
+        val data = LineData(lineDataSet1,lineDataSet2)
+
+        binding.chart.data = data
+        binding.chart.setNoDataText("No data")
+        binding.chart.setNoDataTextColor(resources.getColor(R.color.lightGray))
+        binding.chart.description = description
+        binding.chart.invalidate()
     }
 }
--- app/src/main/java/com/example/tradestat/ui/strategyStatistic/StrategyViewModel.kt
+++ app/src/main/java/com/example/tradestat/ui/strategyStatistic/StrategyViewModel.kt
@@ -1,7 +1,97 @@
 package com.example.tradestat.ui.strategyStatistic
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tradestat.data.TradeDatabase
+import com.example.tradestat.data.TradesRepository
+import com.example.tradestat.data.model.DaysOfWeek
+import com.example.tradestat.data.model.Results
+import com.example.tradestat.data.model.Trade
+import com.github.mikephil.charting.data.Entry
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class StrategyViewModel: ViewModel() {
+class StrategyViewModel(application: Application) : AndroidViewModel(application) {
+    val  RatingList: MutableLiveData<MutableList<Int>> = MutableLiveData()
+    val  mondayList: MutableLiveData<MutableList<Entry>> = MutableLiveData()
+    val  thursdayList: MutableLiveData<MutableList<Entry>> = MutableLiveData()
+    val  tuesdayList: MutableLiveData<MutableList<Entry>> = MutableLiveData()
+    val  wednesdayList: MutableLiveData<MutableList<Entry>> = MutableLiveData()
+    val  fridayList: MutableLiveData<MutableList<Entry>> = MutableLiveData()
+    val  saturdayList: MutableLiveData<MutableList<Entry>> = MutableLiveData()
+    val  sundayList: MutableLiveData<MutableList<Entry>> = MutableLiveData()
+
+    private val repository: TradesRepository
+    init {
+        val tradeDao = TradeDatabase.getDatabase(application).getTradeDao()
+        val strategyDao = TradeDatabase.getDatabase(application).getStrategyDao()
+        val instrumentDao = TradeDatabase.getDatabase(application).getInstrumentDao()
+        repository = TradesRepository(tradeDao,strategyDao,instrumentDao)
+    }
+    fun updateDay(){ // in this method we get coordinates relatively to the trade list
+        viewModelScope.launch(Dispatchers.IO) {
+            val mondayArr  = repository.getDayStatistic(DaysOfWeek.Monday)
+            val thursdayArr  = repository.getDayStatistic(DaysOfWeek.Thursday)
+            val tuesdayArr  = repository.getDayStatistic(DaysOfWeek.Tuesday)
+            val wednesdayArr  = repository.getDayStatistic(DaysOfWeek.Wednesday)
+            val fridayArr  = repository.getDayStatistic(DaysOfWeek.Friday)
+            val saturdayArr  = repository.getDayStatistic(DaysOfWeek.Saturday)
+            val sundayArr  = repository.getDayStatistic(DaysOfWeek.Sunday)
+            getCoordinates(mondayArr,mondayList)
+            getCoordinates(thursdayArr, thursdayList)
+            getCoordinates(tuesdayArr, tuesdayList)
+            getCoordinates(wednesdayArr, wednesdayList)
+            getCoordinates(fridayArr, fridayList)
+            getCoordinates(saturdayArr, saturdayList)
+            getCoordinates(sundayArr, sundayList)
+        }
+    }
+    private fun getCoordinates(
+        tradeArr: List<Trade>,
+        finalList: MutableLiveData<MutableList<Entry>>
+    ) {
+        var list:MutableList<Entry> = mutableListOf()
+        list.add(Entry(0f,0f))
+        var verticalCounter = 0f
+        var horizontalCounter: Float
+        repeat(tradeArr.size) {
+            horizontalCounter = it.toFloat() + 1f
+            if (tradeArr[it].tradeResult == Results.Victory.name){
+                verticalCounter++
+            }else{
+                verticalCounter--
+            }
+            list.add(Entry(horizontalCounter,verticalCounter))
+        }
+        finalList.postValue(list)
+    }
+    fun getRatingList(){ // in this method we get coordinates relatively to the trade list
+        viewModelScope.launch(Dispatchers.IO) {
+            val arr = mutableListOf<Int>()
+            arr.add(getWinRate(DaysOfWeek.Monday))
+            arr.add(getWinRate(DaysOfWeek.Tuesday))
+            arr.add(getWinRate(DaysOfWeek.Wednesday))
+            arr.add(getWinRate(DaysOfWeek.Thursday))
+            arr.add(getWinRate(DaysOfWeek.Friday))
+            arr.add(getWinRate(DaysOfWeek.Saturday))
+            arr.add(getWinRate(DaysOfWeek.Sunday))
+            RatingList.postValue(arr)
+        }
+    }
+    private fun getWinRate(daysOfWeek: DaysOfWeek): Int {
+        val victories  = repository.countTradesByResultAndDate(Results.Victory,daysOfWeek)
+        var defeats  = repository.countTradesByResultAndDate(Results.Defeat,daysOfWeek)
+        if (defeats != 0){
+            return victories*100/(defeats + victories)
+        }
+        if (victories == 0){
+            return 0
+        }
+        return 100
+
+    }
 
 }
