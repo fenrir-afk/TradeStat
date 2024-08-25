32b90eb098f338f6d233302ee4322b8517d40e13
Stepan <Fenrird@inbox.ru>
2024-08-25 20:12:06 +0300

    hide login and reg

--- presentation/src/main/java/com/example/presentation/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/example/presentation/ui/login/LoginActivity.kt
@@ -59,6 +59,8 @@ class LoginActivity : AppCompatActivity() {
         binding.sendToSighUp.setOnClickListener{
             startActivity(Intent(this, RegistryActivity::class.java))
         }
+        binding.sendEmail.setOnClickListener {
+        }
     }
 
     private fun checkCurrentLoginSession() {
